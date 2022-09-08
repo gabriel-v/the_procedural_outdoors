6cc8deb5a147d0f5766e34c275bbad87ebb1264f
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2022-09-08 22:40:46 +0300

    randomize demo render order, skip existing

--- in-container.sh
+++ in-container.sh
@@ -6,10 +6,14 @@ if [[ "t" == "$KUBRIC_USE_GPU" ]]; then nvidia-smi; fi
 rm -rf /tmp/* || true
 find /tmp -type f -delete || true
 
-for x in sky_alt cloud_seed  sky_illum sky_sum_int \
+for x in $(echo sky_alt cloud_seed  sky_illum sky_sum_int \
          sky_sun_elev sky_sun_rot sky_air_density \
          sky_dust_density sky_ozone cloud_thickness \
-         cloud_spread; do
+         cloud_spread | xargs -n1 -P1 echo | sort -R); do
+    if [ -d demo_output/$x ]; then
+        echo "FOLDER EXISTS, SKIPPING: demo_output/$x"
+        continue
+    fi
     time python3 worker.py $x
     mkdir -p demo_output/$x
     mv output/* demo_output/$x
