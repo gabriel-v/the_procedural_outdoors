dfb138ecf80622a77b518e9c143ef1efd51e310d
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2022-09-12 18:39:07 +0300

    fix make-gif script

--- make-gifs.sh
+++ make-gifs.sh
@@ -22,23 +22,23 @@ for x in $(ls $output/pics); do
 done
 
 rm -f $output/pics/concat_*.png || true
-ls $output/pics/rgba_* | grep rgba_ | cut -d_ -f2 | cut -d. -f1 | \
+ls $output/pics/rgba_* | grep rgba_ | rev  | cut -d'/' -f1 | rev | cut -d_ -f2 | cut -d. -f1 | \
 	xargs -I{} -n1 -P8 --verbose \
 	montage -font DejaVu-Sans-Mono -label %t -tile 2x2 -geometry 480x270 \
 	$output/pics/*_{}.png $output/pics/concat_{}.png
 
 echo "montage done"
 
-echo "making gifs..."
-for gif in rgba segmentation depth normal concat; do
-	convert -delay $DELAY -loop 0 $output/pics/${gif}*.png $output/gif/${gif}.gif &
-done
-wait
-echo "gif done"
+# echo "making gifs..."
+# for gif in rgba segmentation depth normal concat; do
+# 	convert -delay $DELAY -loop 0 $output/pics/${gif}*.png $output/gif/${gif}.gif &
+# done
+# wait
+# echo "gif done"
 
 for gif in rgba segmentation depth normal concat; do
 	ffmpeg -y -r $FRAMERATE \
-		-pattern_type glob -i '$output/pics/'$gif'*.png' \
+		-pattern_type glob -i "$output/pics/"$gif'*.png' \
 		-c:v libx264 -vf "fps=24,format=yuv420p"  \
 		$output/vid/$gif.mp4 &
 done
@@ -47,3 +47,11 @@ echo "vids done"
 
 cp $output/gif/concat.gif $output/concat.gif
 cp $output/vid/concat.mp4 $output/concat.mp4
+
+cp $output/gif/rgba.gif $output/rgba.gif
+cp $output/vid/rgba.mp4 $output/rgba.mp4
+
+output_text=$(cat $output/params)
+font_file=/usr/share/fonts/truetype/liberation/LiberationMono-Bold.ttf
+ffmpeg -i $output/rgba.mp4 -vf "drawtext=fontfile=$font_file:text='$output_text':x=100:y=500:fontsize=32:fontcolor=black:box=1:boxcolor=white@0.5: boxborderw=5" -c:a copy $output/marked.mp4
+
